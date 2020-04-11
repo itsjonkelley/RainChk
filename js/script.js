ba29665ee8e7d11e5e033477b7109c261ca11c45
Jonathan Kelley <jonathankelley87@gmail.com>
2020-04-11 14:26:58 -0400

    JK portion of project

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ console.log("found js file!!");
 // $( document ).ready(function() {
 //     console.log( "ready!" );
 // });
+
+
+// var displayToday = moment().format('MMMM Do YYYY'); 
+// var showDate = document.querySelector("#currentDay");
+// showDate.innerHTML=displayToday;
+// console.log(displayToday);
+
+function displayDates () {
+    (showDate.innerHTML=date)
+};
+
+for( let i = 0; i < 5; i++ ) {
+    var date = moment().add(i,'days').format('MMMM Do'); 
+    var showDate = document.querySelector(".day"+i);
+    showDate.innerHTML=date;
+    displayDates(showDate);
+    console.log(i);
+    };
