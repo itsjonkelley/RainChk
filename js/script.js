7f77647db26cae68c474190cbedac5be99b5d0da
dean <dean@SSCCBISHOP.LOCAL>
2020-04-12 21:33:19 -0400

    Extended ImBoredModal to save to imBoredOjc variable

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 //Project 1 for Group 2 GT Full Stack Dev Flex 2020
 console.log("found js file!!");
+var imBoredObj;
 //var url = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={YOUR API KEY}
 const openWeatherapi = {
     key: "ab222fde8a40e718d2b8f92721309596", 
     base: "https://api.openweathermap.org/data/2.5/"
 }
+//Example BoredAPI URL: "http://www.boredapi.com/api/activity?minaccessibility=0&maxaccessibility=0.1"
  const imBoredapi = {
-     base: "http://www.boredapi.com/api/activity?minaccessibility=0&maxaccessibility=0.1"
+     base: "http://www.boredapi.com/api/activity",
+     participants: "participants=",
+     minaccessibility: "minaccessibility=0",
+     maxaccessibility: "maxaccessibility=0.1" 
  }
 
 function imBored() {
@@ -18,28 +23,41 @@ function imBored() {
             .then(function (response) {
                 console.log("URL Sent: ", queryURL);
                 console.log("response: ", response);
-                $(".modal-card-body").html("Activity Type: " + response.type + "<br>" + "Activity: " + response.activity);
-                //$(".model-card-foot").append(<footer class="modal-card-foot"><button class="button modal-close">Cancel</button></footer>);
-            
+                let modalMessage = ("Activity Type: " + response.type + "<br>" + "Activity: " + response.activity + "<br>" + "# of Participants: " + response.participants);
+                $(".modal-card-body").html(modalMessage);
+                    
+                //captures click on "Cancel" button to close modal for "I'm Bored!" link
+                $(document).on("click", "#cancel-btn", function() {
+                    $(".modal").removeClass("is-active");
+                });
+                //captures click to close modal for "I'm Bored!" link
                 $("#modal-close").click(function() {
                     $(".modal").removeClass("is-active");
-                    
-                });        
+                });
+                //captures click to save activity to ToDo list.
+                $(document).on("click", "#save-imbored", function() {
+                    console.log("Save button clicked", response.type);
+                    //saved I'mBored API response to "imBoredObj" to be passed to ToDo list
+                    imBoredObj = response;
+                    console.log("Activity: ", imBoredObj.activity);
+                    $(".modal").removeClass("is-active");
+                });
             });
-            
-        }
-        
-        
-        
-        $("#imbored-modal").on("click", function() {
-            $(".modal").addClass("is-active");
-            imBored();
-            
-            
-    
+    }
+//Captures Click to Next button in I'm Bored Modal            
+$(document).on("click", "#next-imbored", function() {
+    console.log("next button clicked");
+    //Next I'mBored API to be cycled to the next activity
+    imBored();
+});           
+//click funtion to capture click on "I'm Bored!" link             
+$("#imbored-modal").on("click", function() {
+    $(".modal").addClass("is-active");
+    imBored(); 
+});
 
-
- });
+            
+            
 
 
 function displayDates () {
